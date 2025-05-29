245699fe6f55e2fc7cc0089ee3b34bdce6d05c8f
Endy-gamer <gianluca.vendramin10@gmail.com>
2025-05-29 17:00:04 +0200

    z

--- utility/src/main/java/net/mcreator/utility/procedures/LavaEssenceNegativeProcedure.java
+++ utility/src/main/java/net/mcreator/utility/procedures/LavaEssenceNegativeProcedure.java
@@ -2,20 +2,17 @@ package net.mcreator.utility.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 
 import net.mcreator.utility.init.UtilityModEnchantments;
 
-public class LavaEssenceActivateProcedure {
+public class LavaEssenceNegativeProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.getMobType() == MobType.UNDEAD) {
-			if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(UtilityModEnchantments.LAVAESSENCE.get()) == 1) {
-				entity.setSecondsOnFire(100);
-			}
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(UtilityModEnchantments.LAVAESSENCE.get()) == 1) {
+			entity.setSecondsOnFire(100);
 		}
 	}
 }
