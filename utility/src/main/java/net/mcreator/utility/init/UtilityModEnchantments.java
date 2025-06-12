07a6b1a5ab87cab2eb61bbd4ed8f2fede6713e97
Endy-gamer <gianluca.vendramin10@gmail.com>
2025-06-12 17:58:31 +0200

    n

--- utility/src/main/java/net/mcreator/utility/init/UtilityModEnchantments.java
+++ utility/src/main/java/net/mcreator/utility/init/UtilityModEnchantments.java
@@ -12,7 +12,6 @@ import net.minecraft.world.item.enchantment.Enchantment;
 
 import net.mcreator.utility.enchantment.WateressenceEnchantment;
 import net.mcreator.utility.enchantment.ProjectileessenceEnchantment;
-import net.mcreator.utility.enchantment.ObsidianessenceEnchantment;
 import net.mcreator.utility.enchantment.LavaessenceEnchantment;
 import net.mcreator.utility.enchantment.CreeperessenceEnchantment;
 import net.mcreator.utility.UtilityMod;
@@ -21,7 +20,6 @@ public class UtilityModEnchantments {
 	public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, UtilityMod.MODID);
 	public static final RegistryObject<Enchantment> LAVAESSENCE = REGISTRY.register("lavaessence", () -> new LavaessenceEnchantment());
 	public static final RegistryObject<Enchantment> WATERESSENCE = REGISTRY.register("wateressence", () -> new WateressenceEnchantment());
-	public static final RegistryObject<Enchantment> OBSIDIANESSENCE = REGISTRY.register("obsidianessence", () -> new ObsidianessenceEnchantment());
 	public static final RegistryObject<Enchantment> CREEPERESSENCE = REGISTRY.register("creeperessence", () -> new CreeperessenceEnchantment());
 	public static final RegistryObject<Enchantment> PROJECTILEESSENCE = REGISTRY.register("projectileessence", () -> new ProjectileessenceEnchantment());
 }
