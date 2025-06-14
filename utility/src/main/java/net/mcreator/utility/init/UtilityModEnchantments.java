d75685caba81d0d5faab1d6b78f24f959209ef0d
Endy-gamer <gianluca.vendramin10@gmail.com>
2025-06-14 09:53:26 +0200

    n

--- utility/src/main/java/net/mcreator/utility/init/UtilityModEnchantments.java
+++ utility/src/main/java/net/mcreator/utility/init/UtilityModEnchantments.java
@@ -11,7 +11,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.item.enchantment.Enchantment;
 
 import net.mcreator.utility.enchantment.WateressenceEnchantment;
-import net.mcreator.utility.enchantment.ProjectileessenceEnchantment;
 import net.mcreator.utility.enchantment.LavaessenceEnchantment;
 import net.mcreator.utility.enchantment.CreeperessenceEnchantment;
 import net.mcreator.utility.UtilityMod;
@@ -21,5 +20,4 @@ public class UtilityModEnchantments {
 	public static final RegistryObject<Enchantment> LAVAESSENCE = REGISTRY.register("lavaessence", () -> new LavaessenceEnchantment());
 	public static final RegistryObject<Enchantment> WATERESSENCE = REGISTRY.register("wateressence", () -> new WateressenceEnchantment());
 	public static final RegistryObject<Enchantment> CREEPERESSENCE = REGISTRY.register("creeperessence", () -> new CreeperessenceEnchantment());
-	public static final RegistryObject<Enchantment> PROJECTILEESSENCE = REGISTRY.register("projectileessence", () -> new ProjectileessenceEnchantment());
 }
