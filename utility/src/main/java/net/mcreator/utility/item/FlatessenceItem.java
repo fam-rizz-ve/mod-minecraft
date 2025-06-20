7a3baf98de63c98d6cf1b76404d0615f39075957
Endy-gamer <gianluca.vendramin10@gmail.com>
2025-06-20 13:38:42 +0200

    z

--- utility/src/main/java/net/mcreator/utility/item/FlatessenceItem.java
+++ utility/src/main/java/net/mcreator/utility/item/FlatessenceItem.java
@@ -1,11 +1,23 @@
 
 package net.mcreator.utility.item;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
 
 public class FlatessenceItem extends Item {
 	public FlatessenceItem() {
 		super(new Item.Properties().stacksTo(16).rarity(Rarity.EPIC));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, level, list, flag);
+		list.add(Component.translatable("item.utility.flatessence.description_0"));
 	}
 }
